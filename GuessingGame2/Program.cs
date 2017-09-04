36c9ce08736587a78376b9d9d3dac25fc868c589
Codfish97 <cwhiteker@butlercc.edu>
2017-09-04 15:55:30 -0500

    commit

---
+++ GuessingGame2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = 1;
+            int max = 11;
+            int guess = 0;
+            int guesses = 1;
+
+            Random ranNumberGenerator = new Random();
+            int randomNumber;
+            randomNumber = ranNumberGenerator.Next(min, max);
+
+            Console.WriteLine("Guess a number between 1 and 10");
+            guess = Convert.ToInt32(Console.ReadLine());
+
+            while (guess != randomNumber)
+            {
+                if (guess < min)
+                {
+                    Console.WriteLine("Your guess must be higher than 0, guess again");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                }
+                else if (guess > max)
+                {
+                    Console.WriteLine("Your guess must be lower than 10, guess again");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                }
+
+                if (guess < randomNumber)
+                {
+                    Console.WriteLine("Your guess was too low, guess again");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                }
+
+                else if (guess > randomNumber)
+                {
+                    Console.WriteLine("Your guess is too high, guess again");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                }
+
+                guesses++;
+
+                if (guess == randomNumber)
+                    Console.WriteLine("CORRECT! Congratulations, it only took you " + guesses + " guesses");
+            }
+            Console.ReadLine();
+        }
+    }
+}
