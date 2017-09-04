23d1e36ac8dbe45fe40632753850921a39e09c3c
Codfish97 <cwhiteker@butlercc.edu>
2017-09-04 14:56:08 -0500

    commit

---
+++ MarshallsMotto2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarshallsMotto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("M                                                                M");
+            Console.WriteLine("M                                                                M");
+            Console.WriteLine("M                                                                M");
+            Console.WriteLine("M                                                                M");
+            Console.WriteLine("M                                                                M");
+            Console.WriteLine("M                   Make your vision your view                   M");
+            Console.WriteLine("M                                                                M");
+            Console.WriteLine("M                                                                M");
+            Console.WriteLine("M                                                                M");
+            Console.WriteLine("M                                                                M");
+            Console.WriteLine("M                                                                M");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+            Console.ReadLine();
+        }
+    }
+}
