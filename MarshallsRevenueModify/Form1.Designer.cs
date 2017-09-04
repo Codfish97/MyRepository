5a6769077fab4335486b29730e309436f984f254
Codfish97 <cwhiteker@butlercc.edu>
2017-09-04 16:19:02 -0500

    commit

---
+++ MarshallsRevenueModify/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace MarshallsRevenueModify
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMonth = new System.Windows.Forms.TextBox();
+            this.txtExt = new System.Windows.Forms.TextBox();
+            this.txtInt = new System.Windows.Forms.TextBox();
+            this.lblMore = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMonth
+            // 
+            this.txtMonth.Location = new System.Drawing.Point(218, 25);
+            this.txtMonth.Name = "txtMonth";
+            this.txtMonth.Size = new System.Drawing.Size(100, 20);
+            this.txtMonth.TabIndex = 9;
+            // 
+            // txtExt
+            // 
+            this.txtExt.Location = new System.Drawing.Point(112, 25);
+            this.txtExt.Name = "txtExt";
+            this.txtExt.Size = new System.Drawing.Size(100, 20);
+            this.txtExt.TabIndex = 10;
+            // 
+            // txtInt
+            // 
+            this.txtInt.Location = new System.Drawing.Point(6, 25);
+            this.txtInt.Name = "txtInt";
+            this.txtInt.Size = new System.Drawing.Size(100, 20);
+            this.txtInt.TabIndex = 11;
+            // 
+            // lblMore
+            // 
+            this.lblMore.AutoSize = true;
+            this.lblMore.Location = new System.Drawing.Point(6, 90);
+            this.lblMore.Name = "lblMore";
+            this.lblMore.Size = new System.Drawing.Size(0, 13);
+            this.lblMore.TabIndex = 4;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(6, 63);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(0, 13);
+            this.lblTotal.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(215, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Month (1-12)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Exterior Murals";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Interior Murals";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(282, 118);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(97, 23);
+            this.btnCalc.TabIndex = 3;
+            this.btnCalc.Text = "Calculate Total";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 147);
+            this.Controls.Add(this.txtMonth);
+            this.Controls.Add(this.txtExt);
+            this.Controls.Add(this.txtInt);
+            this.Controls.Add(this.lblMore);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMonth;
+        private System.Windows.Forms.TextBox txtExt;
+        private System.Windows.Forms.TextBox txtInt;
+        private System.Windows.Forms.Label lblMore;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCalc;
+    }
+}
+
---
+++ MarshallsRevenueModify/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarshallsRevenueModify
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            int Ext = 750;
+            int Int = 500;
+            int total = 0;
+            int IntAmount = 0;
+            int ExtAmount = 0;
+            int Month = 0;
+
+            Month = Convert.ToInt32(txtMonth.Text);
+            IntAmount = Convert.ToInt32(txtInt.Text);
+            ExtAmount = Convert.ToInt32(txtExt.Text);
+
+            if (Month == 12 || Month == 1 || Month == 2)
+                ExtAmount = 0;
+
+            if (Month == 4 || Month == 5 || Month == 9 || Month == 10)
+                Ext = 699;
+
+            if (Month == 7 || Month == 8)
+                Int = 450;
+            total = (IntAmount * Int) + (ExtAmount * Ext);
+
+            lblTotal.Text = "The total revenue from all the murals painted this month is $" + total + ".00.";
+
+            if (IntAmount > ExtAmount)
+                lblMore.Text = "There were more interior murals painted than exterior murals";
+            else if (ExtAmount > IntAmount)
+                lblMore.Text = "There were more exterior murals painted than exterior murals";
+            else
+                lblMore.Text = "There were exactly the same amount of exterior and interior murals painted, " + IntAmount + ".";
+        }
+    }
+}
