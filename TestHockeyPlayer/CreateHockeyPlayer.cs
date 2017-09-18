85afd2dd31687c20d3d623695f7dddcef94f18d2
Codfish97 <cwhiteker@butlercc.edu>
2017-09-18 15:43:56 -0500

    commit

---
+++ TestHockeyPlayer/CreateHockeyPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TestHockeyPlayer
+{
+    class HockeyPlayer
+    {
+        public string name { get; set; }
+        public int num { get; set; }
+        public int goals { get; set; }
+
+        public HockeyPlayer(string NAME, int NUM, int GOALS)
+        {
+            this.name = NAME;
+            this.num = NUM;
+            this.goals = GOALS;
+        }
+
+        public static void Display(HockeyPlayer player)
+        {
+            Console.WriteLine("Name: {0}", player.name);
+            Console.WriteLine("Jersey #: {0}", player.num);
+            Console.WriteLine("Goals: {0}", player.goals);
+        }
+    }
+}
---
+++ TestHockeyPlayer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHockeyPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HockeyPlayer myHockeyPlayer = new HockeyPlayer("Gretzky", 99, 1000);
+
+            HockeyPlayer.Display(myHockeyPlayer);
+
+            Console.ReadLine();
+        }
+    }
+}
