102053acde9e321d2a111ab2b6ce7aefc95d4638
Codfish97 <cwhiteker@butlercc.edu>
2017-09-18 14:41:19 -0500

    commit

---
+++ CreateTwoBooks/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CreateTwoBooks
+{
+    class Book
+    {
+        private string title;
+        private int numPages;
+        private double price;
+        public string Title
+        {
+            get { return this.title; }
+            set { this.title = value; }
+        }
+        public void AdvertisingMessage()
+        {
+            Console.WriteLine("Buy it now: {0}", this.Title);
+        }
+    }
+}
---
+++ CreateTwoBooks/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateTwoBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book myBook = new Book();
+            Book yourBook = new Book();
+            myBook.Title = "Silas Marner";
+            yourBook.Title = "The Time Traveler's Wife";
+            myBook.AdvertisingMessage();
+            yourBook.AdvertisingMessage();
+            Console.ReadLine();
+        }
+    }
+}
