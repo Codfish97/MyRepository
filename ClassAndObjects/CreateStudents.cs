00bfacfe6d72b0269323254967dc952a9aed980e
Codfish97 <cwhiteker@butlercc.edu>
2017-09-18 14:31:00 -0500

    commit

---
+++ ClassAndObjects/CreateStudents.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace ClassAndObjects
+{
+    class Student
+    {
+        private int idNumber;
+        private string lastName;
+        private double gradePointAverage;
+
+        public const double HIGHEST_GPA = 4.0;
+        public const double LOWEST_GPA = 0.0;
+
+        public int IdNumber { get; set; }
+        public string LastName { get; set; }
+        public double GradePointAverage
+        {
+            get { return gradePointAverage; }
+            set
+            {
+                if (value >= LOWEST_GPA && value <= HIGHEST_GPA)
+                    gradePointAverage = value;
+                else
+                    gradePointAverage = LOWEST_GPA;
+            }
+        }
+
+        public static void Display(Student stu)
+        {
+            Console.WriteLine("{0, 5} {1, -10}{2, 6}", stu.IdNumber, stu.LastName, stu.GradePointAverage.ToString("F1"));
+        }
+    }
+}
---
+++ ClassAndObjects/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student first = new Student();
+            Student second = new Student();
+            first.IdNumber = 123;
+            first.LastName = "Anderson";
+            first.GradePointAverage = 3.5;
+            second.IdNumber = 789;
+            second.LastName = "Daniels";
+            second.GradePointAverage = 4.1;
+
+            Student.Display(first);
+            Student.Display(second);
+
+            Console.ReadLine();
+        }
+    }
+}
