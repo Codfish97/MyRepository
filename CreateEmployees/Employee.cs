11dfbc75627621dbb27fb2f2077d1ed880b46c34
Codfish97 <cwhiteker@butlercc.edu>
2017-09-18 14:50:28 -0500

    commit

---
+++ CreateEmployees/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CreateEmployees
+{
+    class Employee
+    {
+        public int IdNumber { get; set; }
+
+        public double Salary { get; set; }
+        public Employee()
+        {
+            IdNumber = 999;
+            Salary = 0;
+        }
+        public Employee(int empId)
+        {
+            IdNumber = empId;
+            Salary = 0;
+        }
+        public Employee(int empId, double sal)
+        {
+            IdNumber = empId;
+            Salary = sal;
+        }
+        public Employee(char code)
+        {
+            IdNumber = 111;
+            Salary = 100000;
+        }
+
+    }
+}
---
+++ CreateEmployees/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateEmployees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee aWorker = new Employee();
+            Employee anotherWorker = new Employee(234);
+            Employee theBoss = new Employee('A');
+
+            Console.WriteLine("{0, 4}{1, 14}", aWorker.IdNumber,aWorker.Salary.ToString("C")); 
+            Console.WriteLine("{0, 4}{1, 14}", anotherWorker.IdNumber, anotherWorker.Salary.ToString("C")); 
+            Console.WriteLine("{0, 4}{1, 14}", theBoss.IdNumber, theBoss.Salary.ToString("C"));
+
+            Console.ReadLine();
+
+        }
+    }
+}
