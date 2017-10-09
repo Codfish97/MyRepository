3a57df5e294fa81aa678230566874b7bbc0f4c98
Codfish97 <cwhiteker@butlercc.edu>
2017-10-09 18:03:33 -0500

    commit

---
+++ EventExample/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += button_MyOtherClick;
+
+        }
+
+        private void button_MyOtherClick(object sender, EventArgs e)
+        {
+            myLabel1.Text = "Hello again!";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myLabel.Text = "Hello World";
+        }
+    }
+}
